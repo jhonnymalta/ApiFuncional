0a435021f58ee60a1be63b269373da0dc82f9397
jhonnymalta <jhonatamalta@hotmail.com>
2023-10-02 21:40:25 -0300

    First Commit

---
+++ Data/ApiDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApiFuncional.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options) { }
+        
+    }
+}
