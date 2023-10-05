ba7b493ec368ca534b572b31b88a453c2623adf1
jhonnymalta <jhonatamalta@hotmail.com>
2023-10-04 22:39:38 -0300

    Startin generate authentication jwt

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,51 @@
+﻿using ApiFuncional.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace ApiFuncional.Controllers
+{
+    [ApiController]
+    [Route("api/conta")]
+    public class AuthController : ControllerBase
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly JwtSettings _jwtSettings;
+        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager, IOptions<JwtSettings> jwtSettings)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _jwtSettings = jwtSettings.Value;
+        }
+
+        [HttpPost("registrar")]
+        public async Task<ActionResult>  Register(RegisterUserViewModel registerUser)
+        {
+            if (!ModelState.IsValid) return ValidationProblem(ModelState);
+
+            var user = new IdentityUser
+            {
+                UserName = registerUser.Email,
+                Email = registerUser.Email,
+                EmailConfirmed = true
+
+            };
+            var result = await _userManager.CreateAsync(user, registerUser.Password);
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return Ok(GerarJwt());
+            }
+
+            return Ok();
+        }
+
+
+        [HttpPost("Login")]
+        public async Task<ActionResult> Login()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Models/RegisterUserViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiFuncional.Models
+{
+    public class RegisterUserViewModel
+    {
+        [Required(ErrorMessage ="The field {0} is required.")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "The field {0} is required.")]
+        [StringLength(100, ErrorMessage = "The field {0} need {2} and {1} character", MinimumLength = 5)]
+        public string Password { get; set; }
+
+
+        [Compare("Password",ErrorMessage ="Passwords dont match.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class LaginUserViewModel
+    {
+        [Required(ErrorMessage = "The field {0} is required.")]
+        [EmailAddress(ErrorMessage ="The fields format doesnt correct")]
+        public string Email { get; set; }
+        [StringLength(100, ErrorMessage = "The field {0} need {2} and {1} character", MinimumLength = 5)]
+        [Required(ErrorMessage = "The field {0} is required.")]
+
+        public string Password { get; set; }
+    }
+}
